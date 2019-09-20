e88e565acf178a988d05ebee4278424103f0c220
imotyashok <imotyashok@luc.edu>
2019-09-19 20:48:32 -0500

    updated maze

--- maze.c
+++ maze.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-int loadFile(char* filename);
+FILE* loadFile(char* filename);
 int getRowCount(FILE* fp);
 int getColCount(FILE* fp);
-void CreateGrid(char* filename,FILE* fp, int rowCount, int colNum);
+char **inputMaze(FILE* fp, int rowCount, int colNum);
+int **outMaze(char** maze, FILE* fp, int rowCount, int colNum);
 
-int loadFile(char* filename){
+FILE* loadFile(char* filename){
      
      FILE *fp;
      fp = fopen(filename, "r");
@@ -18,17 +17,12 @@ int loadFile(char* filename){
     }
     else{
         printf("FILE LOADED...\n\n");
-        int colNum = getColCount(fp);
-        int rowCount = getRowCount(fp);
+
         fclose(fp); //close file
         fp = fopen(filename, "r");   //reopen
-        CreateGrid(filename,fp, rowCount, colNum);
-        
-       
-        
-        
-        
-        return 0;
+
+
+        return fp;
         
     }
     
@@ -57,36 +51,111 @@ int getRowCount(FILE* fp){
         }
 }
 return Rowcount;
-    
-    
+
 }
 
-void CreateGrid(char* filename, FILE* fp, int rowCount, int colNum){
-    int i, j;
-  
 
-     char maze[rowCount][colNum];
-        for (i = 0; i < rowCount; i++) {
-           
-             for (j = 0; j < colNum; j++) {
-                    char brick = getc(fp);
-                
+
+int **outMaze(char** maze, FILE * fp, int rowCount, int colNum){
+
+int **mazeOut = (int**)malloc(rowCount*sizeof(int*));
+for (int i=0; i<rowCount; i++){
+	mazeOut[i] = (int*)malloc(colNum+1*sizeof(int));
+
+
+}
+
+for (int i=0; i<rowCount; i++){
+	for(int j=0; j< colNum; j++){
+		if (maze[i][j] == "b"){
+		mazeOut[i][j] = -1;
+
+	}
+		if (maze[i][j] == "s"){
+    		mazeOut[i][j] = 0;
+
+
+}
+
+printf("%c", mazeOut[i][j]);
+
+}
+
+}
+return mazeOut;
+
+
+
+
+}
+
+
+
+
+char **inputMaze(FILE* fp, int rowCount, int colNum){
+
+
+
+char **maze = (char**)malloc(rowCount*sizeof(char*));
+for (int i=0; i<rowCount; i++){
+	maze[i] = (char*)malloc(colNum+1*sizeof(char));
+
+
+}
+	char brick;
+        for (int i = 0; i < rowCount; i++) {
+
+             for (int j = 0; j < colNum+1; j++) {
+                    brick = getc(fp);
+
                      maze[i][j] = brick;
-                     
-                    
-                  
-                
-                  printf("%c", maze[i][j]);
-        }
-    }
-   
+
+
 
 }
+    }
+
+for (int i=0; i<rowCount; i++){
+	for (int j=0; j<colNum; j++){
+		printf("%c", maze[i][j]);
+}
+
+printf("\n");
+}
+
+return maze;
+
+}
+
+
+
+
+
+
+
+
+
 
 
 int main()
 {
 char *c = "maze.txt";
-loadFile(c);
-  return 0;
+FILE *fp= loadFile(c);
+
+int colNum = getColCount(fp);
+int rowCount = getRowCount(fp);
+
+printf("%d, %d\n", rowCount, colNum);
+
+char **inMaze = inputMaze(fp,rowCount, colNum);
+
+//int **outmaze = outMaze(inMaze,fp, rowCount, colNum);
+
+fclose(fp);
+
+ return 0;
 }
+int colNum = getColCount(fp);
+        int rowCount = getRowCount(fp);
+int colNum = getColCount(fp);
+        int rowCount = getRowCount(fp);
