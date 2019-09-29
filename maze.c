9c04fa07643ae40a64fa2fb3e17233ea29db0c32
ddpalacios <45171403+ddpalacios@users.noreply.github.com>
2019-09-29 13:00:10 -0500

    Update maze.c

--- maze.c
+++ maze.c
@@ -1,7 +1,8 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
-
+#include <stdbool.h>
 FILE* loadFile(char* filename);
 char **inputMaze(FILE* fp, int rowCount, int colNum);
 int **outputMaze(FILE *fp, int rowCount, int colNum, char **in_maze);
@@ -9,7 +10,7 @@ int getRowCount(FILE *fp, char *filename);
 int getColCount(FILE *fp, char *filename);
 void Print_inMaze(int rows, int col, char **maze);
 void Print_outMaze(int rowCount,int colNum, int** out_maze);
-void solve_maze(int row, int column, char**in_maze, int** out_maze,FILE* journal_fp);
+void solve_maze(int row, int column, char**in_maze, int** out_maze,FILE* journal_fp, FILE* fp, char filename);
 int *get_start_cordinates(char** in_maze,int rows,int columns);
 void solve_by_recursion(int x_pos,int y_pos, int **maze_out, int distance,int rows,int columns,FILE*fp);
 int isOpen(int testRow,int testCol,int row, int col);
@@ -17,8 +18,19 @@ void go_left(int** maze_out, int x_pos, int y_pos, int distance, int rows, int c
 void go_right(int** maze_out, int x_pos, int y_pos, int distance, int rows, int columns,FILE*fp);
 void step_up(int** maze_out,int x_pos, int y_pos, int distance, int rows, int columns,FILE*fp);
 void step_down(int** maze_out, int x_pos, int y_pos, int distance, int rows, int columns,FILE*fp);
-void test();
 void complete_maze_solver(FILE* fp,FILE* journal_fp ,char* filename);
+void read_ints(FILE* replay_fp, FILE* fp, char filename, int x_pos, int y_pos, int** maze_out,char** maze_in,int rows, int columns);
+void show_results(int row, int columns, int**maze_out);
+struct Move{
+
+	int Add,Subtract;
+
+}move = {1,-1};
+
+struct journal_replay{
+	bool replay;
+
+}Replay={false};
 
 
 FILE* loadFile(char*filename){
@@ -70,7 +82,7 @@ int **outputMaze(FILE *fp, int rowCount, int colNum, char**in_maze){
         }
                 for (int i=0; i < rowCount; i ++){
                   for (int j=0; j<colNum; j++){
-                if (in_maze[i][j] == ' ')
+			if (in_maze[i][j] == ' ')
                     out_maze[i][j] = -1;
                     if (in_maze[i][j] == 'b')
                     out_maze[i][j] = -2;
@@ -90,7 +102,7 @@ void Print_outMaze(int row, int col, int **out_maze){
         {
             printf(" %d ",out_maze[i][j]);
         }
-        printf("\n");
+ 	printf("\n");
       }
 
 
@@ -140,6 +152,7 @@ int getColCount(FILE *fp, char *filename)
   colCount = colCount-1;
   return colCount;
 }
+
 int* get_start_cordinates(char** in_maze,int rows,int columns){
 int *starting_pos = malloc(3* sizeof(int));
 //Allocating 1-D Array
@@ -163,101 +176,243 @@ for (int i=0; i<3;i++){
 
 
 }
-int position_is_open(int testRow,int testCol,int row, int col)
-{
-  if (testRow<0 || testRow>=row)
-    return 0;
-  if (testCol<0 || testCol>=col)
-    return 0;
-return 1;
-}
+
 void step_up(int** maze_out, int x_pos, int y_pos, int distance, int rows, int columns,FILE*fp){
    if (maze_out[x_pos-1][y_pos]==-1)
        {
+	//Every maze is located somewhere at row 0. Thus creating condition to recognize its exit.
+	if (x_pos-1 == 0){
+	fprintf(fp,"%d\n\n\n", move.Subtract); //Recall our sturct to record its directions
+	++distance;
+	maze_out[x_pos-1][y_pos] = distance;
+	show_results(rows,columns,maze_out); //Prints out our resulted INT maze with its updated postions the algorithim took and exits program
 
-        fprintf(fp, "Step Up at --> <Row: %d> <columns: %d>\t\t<Prev Cell: %d> <New Cell: %d>\n", x_pos-1, y_pos, distance, distance+1);
-        if (x_pos-1 == 0){
-        fprintf(fp, "\n\t\t\t---SOLVED---\t\t\t\n");
-        fprintf(fp, " \n\t\tDetecting other possible paths...\t\n\n");
-        }
-
-         solve_by_recursion(x_pos-1,y_pos, maze_out, distance+1,rows,columns, fp);
+	}
+	else{
+	 fprintf(fp,"%d\n", move.Subtract);
+         solve_by_recursion(x_pos-1,y_pos, maze_out, distance+1,rows,columns, fp); //Recursively updating our parameters.  Consider lessening # of parameters
        }
+}
 
 }
 void step_down(int** maze_out, int x_pos, int y_pos, int distance, int rows, int columns,FILE*fp){
     if (maze_out[x_pos+1][y_pos]==-1)
        {
-        fprintf(fp, "Step Down at --> <Row: %d> <columns: %d>\t\t\t<Prev Cell: %d> <New Cell: %d>\n", x_pos-1, y_pos, distance, distance+1);
+	  fprintf(fp,"%d\n", move.Add);
           solve_by_recursion(x_pos+1,y_pos,maze_out,distance+1,rows,columns,fp);
-       }
+      }
 
 
 }
 void go_left(int** maze_out, int x_pos, int y_pos, int distance, int rows, int columns,FILE*fp){
  if (maze_out[x_pos][y_pos-1]==-1)
-         {
-        fprintf(fp, "Left at --> <Row: %d> <columns: %d>\t\t\t<Prev Cell: %d> <New Cell: %d>\n", x_pos-1, y_pos, distance, distance+1);
-         solve_by_recursion(x_pos,y_pos-1,maze_out,distance+1,rows,columns,fp);
+	 {
+
+	 fprintf(fp,"%d\n", move.Subtract);
+	 solve_by_recursion(x_pos,y_pos-1,maze_out,distance+1,rows,columns,fp);
        }
 
 
 }
 void go_right(int** maze_out, int x_pos, int y_pos, int distance, int rows, int columns,FILE*fp){
 
+
       if (maze_out[x_pos][y_pos+1]==-1)
             {
-              fprintf(fp, "Right at --> <Row: %d> <columns: %d>\t\t\t<Prev Cell: %d> <New Cell: %d>\n", x_pos-1, y_pos, distance, distance+1);
+	      fprintf(fp,"%d\n", move.Add);
+
               solve_by_recursion(x_pos,y_pos+1,maze_out,distance+1,rows,columns,fp);
             }
 
 }
 void solve_by_recursion(int x_pos,int y_pos, int **maze_out, int distance,int rows,int columns,FILE*fp){
-  int curr_body_pos = maze_out[x_pos][y_pos];
-  if (curr_body_pos == -3 || curr_body_pos == -1 ||curr_body_pos < distance){
+/* There are a few things to take into consideration here.
+	We have to prevent the algorithim to revist cells,
+	update out positions, 
+	and give directions on where to move. (In our case that is N. E. S. W.)
+*/
+
+
+//This condition allows the algorithim prevent revisiting previous cells and updating our current maze
+int curr_body_pos = maze_out[x_pos][y_pos];
+ if (curr_body_pos == -3 || curr_body_pos == -1 ||curr_body_pos < distance){
     maze_out[x_pos][y_pos] = distance;
   }
-
-   if (position_is_open(x_pos -1,y_pos,rows,columns))
-        step_up(maze_out, x_pos, y_pos, distance,rows, columns, fp);
-
-   if (position_is_open(x_pos+ 1,y_pos,rows,columns))
-       step_down(maze_out, x_pos, y_pos,distance, rows, columns,fp);
-
-   if (position_is_open(x_pos,y_pos+1,rows,columns))
-       go_right(maze_out, x_pos, y_pos, distance,rows, columns,fp);
-
-   if (position_is_open(x_pos,y_pos-1,rows,columns))
-      go_left(maze_out, x_pos, y_pos,distance, rows, columns,fp);
+	//Here are our directions. 
+	//Each direction holds a condition to know when a cell is empty or not.
+	 step_up(maze_out, x_pos, y_pos, distance,rows, columns, fp);
+	 step_down(maze_out, x_pos, y_pos,distance, rows, columns,fp);
+	 go_right(maze_out, x_pos, y_pos, distance,rows, columns,fp);
+         go_left(maze_out, x_pos, y_pos,distance, rows, columns,fp);
 
 }
 
 
-void solve_maze(int rows, int columns, char** maze_in, int** maze_out, FILE* journal_fp)
+void solve_maze(int rows, int columns, char** maze_in, int** maze_out, FILE* journal_fp, FILE* fp, char filename)
 {
+	/*
+	As soon as we reached this function, we grab all the information we need
+	 for our general maze from its text file. As you can see, we are calling the .replay var from
+	struct to indicate whether or not we are solving the maze with the recursive algorithim or the
+	journal itself.
+	 */
+
     int *starting_pos = get_start_cordinates(maze_in, rows, columns);
     int x_pos = starting_pos[0];
     int y_pos = starting_pos[1];
 
-    solve_by_recursion(x_pos, y_pos, maze_out, 0,rows,columns, journal_fp);
-    Print_outMaze(rows,columns, maze_out);
-    printf("\nStaring Position--> x: %d, y: %d\n", x_pos, y_pos);
-    printf("\n\nThis maze was recorded under .journal file\n");
-    printf("\n\nTo View:  ./yourmazefilename -j yourjournalname\n\n");
+	if (Replay.replay){ //If this is True then solve maze using the journals instructions
+		printf("Replaying journal...\n");
+		read_ints(journal_fp, fp, filename, x_pos,y_pos,maze_out, maze_in,rows,columns);
+		exit(0);
+
+		}else{ //otherwise solve it recursively
+    		  solve_by_recursion(x_pos, y_pos, maze_out, 0,rows,columns, journal_fp);
+		  fprintf(journal_fp,"\n\n");
+     		  printf("\nStaring Position--> x: %d, y: %d\n", x_pos, y_pos);
 
 
+}
 
 }
 
 
 void complete_maze_solver(FILE* fp,FILE* journal_fp, char* filename){
-        int columns = getColCount(fp,filename);
+	//This is our general maze data that needs to be collected
+	int columns = getColCount(fp,filename);
         int rows = getRowCount(fp, filename);
         char** in_maze = inputMaze(fp,rows,columns);
         int** out_maze = outputMaze(fp,rows,columns, in_maze);
-        solve_maze(rows,columns, in_maze,out_maze, journal_fp);
+
+	/*Once we have gathered our information,
+	 our next method will set up our coordinates to solve 
+	for 'training' or 'testing'
+	*/
+        solve_maze(rows,columns, in_maze,out_maze, journal_fp, fp,filename);
 
 }
+
+
+void read_ints(FILE* replay_fp, FILE* fp, char filename,int x_pos, int y_pos, int** maze_out,char** maze_in, int rows, int columns){
+
+/*
+
+So now that we have all the general information needed, we are now able to
+'solve' the maze using the journal's instructions...
+
+Remember,  -1 --> indicates movement is subtracting row or col
+	   +1 --> indicates movement is adding a row or col
+
+
+for instructions in journal:
+	if (instructions == -1):
+		Go_left || Go_up if positionIsOpen();
+	if (instuctions == 1):
+		Go_right || Go_down if positionIsOpen();
+
+
+*/
+int instruction;
+char journal_contents;
+int distance = 0;
+printf("Starting at: xpos: %d, ypos: %d\n\n",x_pos,y_pos);
+
+
+/*
+At this point of our code, as we can clearly see that this code is a bit messy.
+
+Lets try adding function 'Update();' in order to see less code and have it run more effitently.
+
+Meaning, the practice of preventing repeating code.
+
+
+However, these conditions follow the intructions of our .journal file.
+While it is running, we are able to see how our testing maze is being updated accoding to its instructions on
+how and where to move.
+*/
+
+printf("\n\n\t---Reading Instructions to solve... Please Wait...\n\n");
+//While loop reads through entire .journal file as an INT
+fscanf(replay_fp, "%d", &instruction);
+while(!feof(replay_fp) && journal_contents != EOF){
+
+//Checks neighbors... again,  it is not neccessary to repeat code.
+//Create function 'Check_Neighbors();'
+int curr_body_pos = maze_out[x_pos][y_pos];
+ if (curr_body_pos == -3 || curr_body_pos == -1 ||curr_body_pos < distance){
+    maze_out[x_pos][y_pos] = distance;
+  }
+
+/*
+	if (instruction == -1){
+	printf("Checking to go Up or left...\n\n");
+	}
+	else{
+	printf("Checking to go Down or right...\n\n");
+	}
+*/
+	sleep(1);
+
+
+
+	if (instruction == -1 && maze_out[x_pos-1][y_pos]==-1 ){
+		--x_pos;
+		if (x_pos == 0){
+		printf("\nSolved! Found exit! At row: %d and col: %d\n\n",x_pos,y_pos);
+		Print_inMaze(rows,columns, maze_in);
+		exit(0);
+		}
+		++distance;
+		maze_in[x_pos][y_pos] = '*';
+		Print_inMaze(rows,columns, maze_in);
+
+		printf("\tDecided to go up at x: %d and y: %d\n\n", x_pos, y_pos);
+	}else if (instruction == -1 && maze_out[x_pos][y_pos-1]==-1){
+		--y_pos;
+		++distance;
+		maze_in[x_pos][y_pos] = '*';
+		Print_inMaze(rows,columns, maze_in);
+		printf("\tDecided to go left at x: %d and y: %d\n\n", x_pos, y_pos);
+	}else if (instruction == 1 && maze_out[x_pos][y_pos+1] == -1){
+		++y_pos;
+		++distance;
+		maze_in[x_pos][y_pos] = '*';
+		Print_inMaze(rows,columns, maze_in);
+		printf("\tDecided to go right at x: %d and y: %d\n\n", x_pos, y_pos);
+	}else if(instruction == 1 && maze_out[x_pos+1][y_pos]== -1){
+		++x_pos;
+		++distance;
+		maze_in[x_pos][y_pos] = '*';
+		Print_inMaze(rows,columns, maze_in);
+		printf("\tDecided to go down at x: %d and y: %d\n\n", x_pos, y_pos);
+	}
+
+
+
+	fscanf(replay_fp, "%d", &instruction);
+	journal_contents = getc(replay_fp); //Allows to detect end of file
+
+
+   }
+
+	if (x_pos != 0){
+		printf("\nJournal Replay could not find exit for this maze.\n\n\tContinue by solve recursively?(1/0)\n\n");
+		sleep(2);
+	//	solve_by_recursion(x_pos, y_pos, maze_out, 0,rows,columns, replay_fp);         #Clean code before solve recursively
+	}
+
+}
+
+
+
+
+
+void show_results(int row, int columns, int** maze_out){
+   printf("SOLVED AND RECORDED\n\n");
+   Print_outMaze(row,columns, maze_out);
+    exit(0);
+}
+
 
 int main(int argc, char **argv){
   FILE* fp = NULL;
@@ -265,7 +420,6 @@ int main(int argc, char **argv){
   char* filename[100];
   char* replay_file;
   char* filename_journal;
-  char journal_contents;
   FILE* replay_fp;
   FILE* journal_fp = NULL; //Just for testing on linux Nvidia
 
@@ -278,29 +432,37 @@ int main(int argc, char **argv){
                         journal_fp --> where you would write journal usage in
                         -j loads journal from previous run and replays changes in maze
                         */
-                    printf("Enter Maze File:\n> ");
-                    scanf("%s",filename);
-                    fp = fopen(filename, "r");
-                                filename_journal = optarg;
+
+				filename_journal = optarg;
                                 journal_fp = fopen(filename_journal, "a+");
+
+				printf("Enter Maze File To Train With:\n> ");
+  				scanf("%s",filename);
+  				fp = fopen(filename, "a+");
+
+
                                 break;
+
                         case 'j':
-                                printf("\n\t\t\tREPLAYS MAZE UPDATES\n\n");
-                    replay_file = optarg;
-                    replay_fp = fopen(replay_file, "r");
-                    while (journal_contents !=EOF){
-                                    printf("%c", journal_contents);
-                        journal_contents = getc(replay_fp);
-                        }
-                    exit(0);
+			filename_journal = optarg;
+			journal_fp = fopen(filename_journal,"r"); //Allows access to journal file
+			/*
+			Once we take users flag input, we also need to opt the user for
+			the new maze to solve using the journals instructions, so lets create that option.
+
+			--> Consider creating a method so we can avoid repetition
+			*/
+			printf("Enter Maze File To Test With:\n> ");
+			scanf("%s",filename);
+			fp = fopen(filename, "a+");
+			Replay.replay = true;
+			complete_maze_solver(fp, journal_fp,filename);
                         }
 
-        }if (fp == NULL){
-              printf("\n\nNo MAZE FILE detected.  missing -o ?\nHint: Add -o and create a journal file\n\n");
-              exit(0);
-              }
-        else{
-        complete_maze_solver(fp, journal_fp,filename);
         }
+
+        complete_maze_solver(fp, journal_fp,filename);
+
+
   return 0;
 }
